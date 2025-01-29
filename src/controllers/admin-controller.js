b2b89370eaa46c2a305ad4d2328b84f15876f22f
Boris Todorov <btodorovtodorov@gmail.com>
2025-01-29 15:16:47 +0200

    add login functionality for admin

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,12 +1,28 @@
 import { Router } from 'express';
 import contactHandler from '../handlers/contact-handler.js';
+import 'dotenv/config';
 
 const adminController = Router();
+
+const adminEmail = process.env.ADMIN_EMAIL;
+const adminPass = process.env.ADMIN_PASSWORD;
+let isAuthenticated = false;
+
+adminController.post('/admin', async (req, res) => {
+    const admin = req.body;
+
+    if (admin.email === adminEmail && admin.password === adminPass) {
+        isAuthenticated = true;
+        return res.redirect('/admin');
+    } else {
+        return res.render('admin', { error: 'Invalid email or password!' });
+    }
+});
 
 adminController.get('/admin', async (req, res) => {
     let contacts = await contactHandler.getAllContacts();
 
-    res.render('admin', { contacts });
+    res.render('admin', { contacts, isAuthenticated });
 });
 
 export default adminController;
---
+++ src/handlers/admin-handler.js
@@ -0,0 +1,1 @@
+import Admin from '../Models/Admins.js';
